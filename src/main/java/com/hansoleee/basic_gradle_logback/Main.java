8ee434f1672f1d25257f552a20701c761ecc5b98
이한솔 <lee.hs@hepi.co.kr>
2022-06-23 00:08:16 +0900

    first commit

---
+++ src/main/java/com/hansoleee/basic_gradle_logback/Main.java
@@ -0,0 +1,11 @@
+package com.hansoleee.basic_gradle_logback;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("haha");
+    }
+}
